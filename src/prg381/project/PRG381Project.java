d001246468d1613625c9db08f361fa82c94be626
Nkosinathi-Mabena <nmabena.nm22@gmail.com>
2024-08-27 14:17:20 +0200

    milestone 2

---
+++ src/prg381/project/PRG381Project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prg381.project;
+
+/**
+ *
+ * @author nmabe
+ */
+public class PRG381Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
