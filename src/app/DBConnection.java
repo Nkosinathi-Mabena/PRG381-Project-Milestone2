1a301b9297b1b66a9ee3a990266683805d1b1316
Felix-Red <felix.mandyme126@gmail.com>
2024-08-28 12:31:21 +0200

    content for DBConnection

--- src/app/DBConnection.java
+++ src/app/DBConnection.java
@@ -8,6 +8,39 @@ package app;
  *
  * @author felix
  */
+import java.sql.*;
+
+
 public class DBConnection {
+    private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+    
+    private static final String JDBC_URL = "jdbc:derby:libraryDB; create =true";
+    
+    Connection con;
+    public DBConnection(){}
+    
+    public void connect() throws ClassNotFoundException {
+        try {
+            Class.forName(DRIVER);
+            this.con = DriverManager.getConnection(JDBC_URL);
+            if (this.con != null) {
+                System.out.println("Connected to database");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void createTable() {
+        try (Statement stmt = con.createStatement()) {
+            String query = "CREATE TABLE Books(StudentID VARCHAR(20),"
+                    + " name VARCHAR(20), surname VARCHAR(20))";
+            stmt.executeUpdate(query);
+            System.out.println("Table created");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("Table creation failed");
+        }
+    }
+    
     
 }
